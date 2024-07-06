b1ae05330d3dfb8009640debd3c966e2a17d676b
junaeddurjoy <junaed.durjoy@gmail.com>
2024-07-06 16:14:41 +0600

    final draft -3

--- src/app/page.js
+++ src/app/page.js
@@ -51,7 +51,7 @@ const page = () => {
               <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                 <div className="w-10 rounded-full">
                   <img alt="Tailwind CSS Navbar component"
-                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                    src="https://avatars.githubusercontent.com/u/66216291?v=4" />
                 </div>
               </div>
               <ul
